dd1eb1592cba0c02f482dadaf581b7e40eed3316
vahide <vahidefarzane@gmail.com>
2022-11-09 13:37:45 +0330

    Portal created

--- src/Component/Products/Products.js
+++ src/Component/Products/Products.js
@@ -2,6 +2,7 @@ import React from "react";
 import Errorbox from "../Errorbox/Errorbox";
 import AddNewProducts from "../AddNewProducts/AddNewProducts";
 import ProductsTable from "../ProductsTable/ProductsTable";
+import DeleteModal from "../DeleteModal/DeleteModal";
 
 export default function Products() {
   return (
--- src/Component/ProductsTable/ProductsTable.js
+++ src/Component/ProductsTable/ProductsTable.js
@@ -1,84 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteModal from "../DeleteModal/DeleteModal";
 import "./ProductsTable.css";
 
 export default function ProductsTable() {
-  return (
-    <table className="products-table">
-      <tr className="products-table-heading-tr">
-        <th>عکس</th>
-        <th>اسم</th>
-        <th>قیمت</th>
-        <th>موجودی</th>
-      </tr>
+  const [isShowModal, setIsShowModal] = useState(false);
 
-      <tr className="products-table-tr">
-        <td>
-          <img
-            src="/img/buttle.jpeg"
-            alt="buttle"
-            className="products-table-img"
-          />
-        </td>
-        <td>بطری</td>
-        <td>56000 تومان</td>
-        <td>82</td>
-        <td>
-          <button className="products-table-btn">جزییات</button>
-          <button className="products-table-btn">حذف</button>
-          <button className="products-table-btn">ویرایش</button>
-        </td>
-      </tr>
-      <tr className="products-table-tr">
-        <td>
-          <img
-            src="/img/Headphone.jpeg"
-            alt="Headphone"
-            className="products-table-img"
-          />
-        </td>
-        <td>هدفون</td>
-        <td>200,000 تومان</td>
-        <td>2</td>
-        <td>
-          <button className="products-table-btn">جزییات</button>
-          <button className="products-table-btn">حذف</button>
-          <button className="products-table-btn">ویرایش</button>
-        </td>
-      </tr>
-      <tr className="products-table-tr">
-        <td>
-          <img
-            src="/img/kard.jpg"
-            alt="Headphone"
-            className="products-table-img"
-          />
-        </td>
-        <td>چاقو</td>
-        <td>300,000 تومان</td>
-        <td>18</td>
-        <td>
-          <button className="products-table-btn">جزییات</button>
-          <button className="products-table-btn">حذف</button>
-          <button className="products-table-btn">ویرایش</button>
-        </td>
-      </tr>
-      <tr className="products-table-tr">
-        <td>
-          <img
-            src="/img/notebook.jpg"
-            alt="Headphone"
-            className="products-table-img"
-          />
-        </td>
-        <td>دفترچه</td>
-        <td>40,000 تومان</td>
-        <td>32</td>
-        <td>
-          <button className="products-table-btn">جزییات</button>
-          <button className="products-table-btn">حذف</button>
-          <button className="products-table-btn">ویرایش</button>
-        </td>
-      </tr>
-    </table>
+  const deletBtnHamdeler = () => {
+    console.log('khar');
+    setIsShowModal(true);
+  };
+
+  const deleteModalCancelAction = () => {
+    console.log("مدال کنسل شد");
+    setIsShowModal(false);
+  };
+
+  const deleteModalSubmitAction = () => {
+    console.log("مدال تایید شد");
+    setIsShowModal(false);
+  };
+
+  return (
+    <>
+      <table className="products-table">
+        <tr className="products-table-heading-tr">
+          <th>عکس</th>
+          <th>اسم</th>
+          <th>قیمت</th>
+          <th>موجودی</th>
+        </tr>
+        <tr className="products-table-tr">
+          <td>
+            <img
+              src="/img/buttle.jpeg"
+              alt="buttle"
+              className="products-table-img"
+            />
+          </td>
+          <td>بطری</td>
+          <td>56000 تومان</td>
+          <td>82</td>
+          <td>
+            <button className="products-table-btn">جزییات</button>
+            <button className="products-table-btn" onClick={deletBtnHamdeler}>
+              حذف
+            </button>
+            <button className="products-table-btn">ویرایش</button>
+          </td>
+        </tr>
+      </table>
+      {isShowModal && (
+        <DeleteModal
+          submitAction={deleteModalSubmitAction}
+          cancelAction={deleteModalCancelAction}
+        />
+      )}
+    </>
   );
 }

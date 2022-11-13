b82d75e3596fbbbc9e12a3a955257c1425c9b6ce
vahide <vahidefarzane@gmail.com>
2022-11-13 15:41:44 +0330

    all btns at product table seted

--- frontend/src/Component/EditModal/EditModal.js
+++ frontend/src/Component/EditModal/EditModal.js
@@ -1,39 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BsCursorText} from 'react-icons/bs'
-import {BiDollarCircle} from 'react-icons/bi'
-import {BsBagCheck} from 'react-icons/bs'
-import {TbNumbers} from 'react-icons/tb'
 import "./EditModal.css";
 
-export default function EditModal({editAction}) {
+export default function EditModal({children}) {
   return ReactDOM.createPortal(
     <div className="modal-parent active">
-      <div className="edit-modal-container">
-        <h1>اطلاعات جدید را وارد کنید</h1>
-        <form action="#">
-          <div className="edit-products-form-group">
-            <BsCursorText className="add-products-icon" />
-            <input type="text" placeholder="اسم محصول را ویرایش کنید" />
-          </div>
-          <div className="edit-products-form-group">
-            <BsBagCheck className="add-products-icon" />
-            <input type="text" placeholder="موجودی محصول را ویرایش کنید" />
-          </div>
-          <div className="edit-products-form-group">
-            <BiDollarCircle className="add-products-icon" />
-            <input type="text" placeholder="قیمت محصول را ویرایش کنید" />
-          </div>
-          <div className="edit-products-form-group">
-            <TbNumbers className="add-products-icon" />
-            <input
-              type="text"
-              placeholder="تعداد رنگ بندی محصول را ویرایش کنید"
-            />
-          </div>
-        </form>
-        <button className="edit-products-btn" onClick={()=>editAction()}>ثبت اطلاعات</button>
-      </div>
+      {children}
     </div>,
     document.getElementById("modals-parent")
   );
--- frontend/src/Component/ProductsTable/ProductsTable.js
+++ frontend/src/Component/ProductsTable/ProductsTable.js
@@ -12,6 +12,13 @@ export default function ProductsTable() {
   const [allProducts, setAllProducts] = useState([]);
   const [productID, setproductID] = useState(null);
   const [mainproduct, setMainproduct] = useState({});
+  const [newProductTitle, setNewProductTitle] = useState("");
+  const [newProductCount, setNewProductCount] = useState("");
+  const [newProductPrice, setNewProductPrice] = useState("");
+  const [newProductImg, setNewProductImg] = useState("");
+  const [newProductPopularity, setNewProductPopularity] = useState("");
+  const [newProductSale, setNewProductSale] = useState("");
+  const [newProductColors, setNewProductColors] = useState("");
 
   useEffect(() => {
     getAllProducts();
@@ -39,8 +46,30 @@ export default function ProductsTable() {
   const closeModalDetails = () => {
     setisShowDetailModal(false);
   };
-  const editModals = () => {
-    setisShowEditModal(false);
+  const editModals = (e) => {
+    e.preventDefault();
+    const productNewInfos = {
+      title: newProductTitle,
+      price: newProductPrice,
+      count: newProductCount,
+      img: newProductImg,
+      popularity: newProductPopularity,
+      sale: newProductSale,
+      colors: newProductColors,
+    };
+    fetch(`http://localhost:8000/api/products/${productID}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(productNewInfos),
+    })
+      .then((res) => res.json)
+      .then((data) => {
+        console.log(data);
+        setisShowEditModal(false);
+        getAllProducts();
+      });
   };
   return (
     <>
@@ -88,7 +117,17 @@ export default function ProductsTable() {
                   </button>
                   <button
                     className="products-table-btn"
-                    onClick={() => setisShowEditModal(true)}
+                    onClick={() => {
+                      setisShowEditModal(true);
+                      setproductID(product.id);
+                      setNewProductTitle(product.title);
+                      setNewProductCount(product.count);
+                      setNewProductPrice(product.price);
+                      setNewProductImg(product.img);
+                      setNewProductPopularity(product.popularity);
+                      setNewProductSale(product.sale);
+                      setNewProductColors(product.colors);
+                    }}
                   >
                     ویرایش
                   </button>
@@ -140,7 +179,90 @@ export default function ProductsTable() {
           </div>
         </DetailsModal>
       )}
-      {isShowEditModal && <EditModal editAction={editModals} />}
+      {isShowEditModal && (
+        <EditModal>
+          <div className="edit-modal-container">
+            <h1>اطلاعات جدید را وارد کنید</h1>
+            <form action="#">
+              <div className="edit-products-form-group">
+                <label className="lable-input">اسم محصول</label>
+                <input
+                  type="text"
+                  placeholder="اسم محصول را بنویسید"
+                  value={newProductTitle}
+                  onChange={(e) => setNewProductTitle(e.target.value)}
+                />
+              </div>
+              <div className="edit-products-form-group">
+                <label className="lable-input">موجودی محصول</label>
+
+                <input
+                  type="text"
+                  placeholder="موجودی محصول را بنویسید"
+                  value={newProductCount}
+                  onChange={(e) => setNewProductCount(e.target.value)}
+                />
+              </div>
+              <div className="edit-products-form-group">
+                <label className="lable-input">میزان محبوبیت</label>
+
+                <input
+                  type="text"
+                  placeholder="میزان محبوبیت محصول را بنویسید"
+                  value={newProductPopularity}
+                  onChange={(e) => setNewProductPopularity(e.target.value)}
+                />
+              </div>
+              <div className="edit-products-form-group">
+                <label className="lable-input">تعداد رنگ بندی</label>
+
+                <input
+                  type="text"
+                  placeholder="تعداد رنگ بندی محصول را بنویسید"
+                  value={newProductColors}
+                  onChange={(e) => setNewProductColors(e.target.value)}
+                />
+              </div>
+              <div className="edit-products-form-group">
+                <label className="lable-input">قیمت محصول</label>
+
+                <input
+                  type="text"
+                  placeholder="قیمت محصول را بنویسید"
+                  value={newProductPrice}
+                  onChange={(e) => setNewProductPrice(e.target.value)}
+                />
+              </div>
+              <div className="edit-products-form-group">
+                <label className="lable-input">آدرس عکس محصول</label>
+
+                <input
+                  type="text"
+                  placeholder="آدرس عکس محصول را بنویسید"
+                  value={newProductImg}
+                  onChange={(e) => setNewProductImg(e.target.value)}
+                />
+              </div>
+              <div className="edit-products-form-group">
+                <label className="lable-input">میزان فروش محصول</label>
+
+                <input
+                  type="text"
+                  placeholder="میزان فروش محصول را بنویسید"
+                  value={newProductSale}
+                  onChange={(e) => setNewProductSale(e.target.value)}
+                />
+              </div>
+              <button
+                className="edit-products-btn"
+                onClick={(e) => editModals(e)}
+              >
+                ثبت اطلاعات
+              </button>
+            </form>
+          </div>
+        </EditModal>
+      )}
     </>
   );
 }
